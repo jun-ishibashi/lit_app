d58e6b0fe52cbde8f756b1bc8ab7db9bf7b4d02d
jun-ishibashi <janvarujan@icloud.com>
2021-07-31 14:42:51 +0900

    ルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "services#index"
   get 'services/search'
-  post 'services/search'
 
-  resources :services do
-    collection do
-      match 'search' => 'services#search', via: [:get, :post]
-    end
-  end
+  resources :services
 
   resources :users, only: :show
   resources :providers, only: :show
